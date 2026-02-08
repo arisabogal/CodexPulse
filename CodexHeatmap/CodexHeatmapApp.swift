b639befb8e6d2d945fa55c56b5d2b5377972d1d4
arisabogal <sabogalari@gmail.com>
2026-02-07 20:14:50 -0800

    Initial Commit

---
+++ CodexHeatmap/CodexHeatmapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodexHeatmapApp.swift
+//  CodexHeatmap
+//
+//  Created by Ari Sabogal on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CodexHeatmapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
